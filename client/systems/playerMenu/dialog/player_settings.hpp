357f388cbe5117c2caf544f962d24f9f97d6ae71
MokeyII <McClainG9616@Gmail.com>
2014-12-30 02:37:59 -0500

    Added and Edited TAW and A3W VD scripts

--- client/systems/playerMenu/dialog/player_settings.hpp
+++ client/systems/playerMenu/dialog/player_settings.hpp
@@ -89,13 +89,13 @@ class playerSettings {
 			w = 0.3; h = 0.05;
 		};
 
-		class distanceText : w_RscText {
+		/*class distanceText : w_RscText {
 			idc = view_range_text;
 			text = "View range:";
 			sizeEx = 0.025;
 			x = 0.03; y = 0.40;
 			w = 0.3; h = 0.02;
-		};
+		};*/
 
 		class uptimeText : w_RscText {
 			idc = uptime_text;
@@ -160,29 +160,29 @@ class playerSettings {
 			w = 0.225; h = 0.033 * safezoneH;
 		};
 
-		class btnDistanceNear : w_RscButton {
+		/*class btnDistanceNear : w_RscButton {
 			idc = -1;
 			text = "Near";
 			onButtonClick = "setViewDistance 1100;";
 			x = 0.02; y = 0.43;
 			w = 0.125; h = 0.033 * safezoneH;
-		};
+		};*/
 
-		class btnDistanceMedium : w_RscButton {
+		/*class btnDistanceMedium : w_RscButton {
 			idc = -1;
 			text = "Medium";
 			onButtonClick = "setViewDistance 2200;";
 			x = 0.02; y = 0.5;
 			w = 0.125; h = 0.033 * safezoneH;
-		};
+		};*/
 
-		class btnDistanceFar : w_RscButton {
+		/*class btnDistanceFar : w_RscButton {
 			idc = -1;
 			text = "Far";
 			onButtonClick = "setViewDistance 3300;";
 			x = 0.02; y = 0.57;
 			w = 0.125; h = 0.033 * safezoneH;
-		};
+		};*/
 
 		class btnDistanceInsane : w_RscButton {
 			text = "Insane";
